f9365f91d4ee8d9cccef1af1df5dc28e0ca3067b
Asibur Rahman <asibur70@gmail.com>
2025-10-15 21:24:49 +0600

    add redux to manage counter state smoothly

--- redux_react/src/App.jsx
+++ redux_react/src/App.jsx
@@ -1,47 +1,24 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import Counter from './components/Counter'
 import Starts from './components/Starts'
+import { decrement, increment } from './features/counters/counterSlice'
 
-const initialCounters =[
-  {
-    id: 1,
-    value: 0
-  },
-  {
-    id: 2,
-    value: 0
-  }
-]
+
 
 function App() {
-  const [counters, setCounters] =useState(initialCounters)
-  const handleIncrement = (counterId)=>{
-    const updatedCounters = counters.map(counter=>{
-        if (counter.id === counterId) {
-          return{
-            ...counter,
-            value: counter.value + 1
-          }
-        }
-        return counter
-      })
-      setCounters(updatedCounters)
+  const counters = useSelector((state) => state.counters)
+  const dispatch = useDispatch()
+  const handleIncrement = (counterId) => {
+    dispatch(increment(counterId))
+
   }
-  const handleDecrement = (counterId)=>{
-    const updatedCounters = counters.map(counter=>{
-        if (counter.id === counterId) {
-          return{
-            ...counter,
-            value: counter.value - 1
-          }
-        }
-        return counter
-      })
-      setCounters(updatedCounters)
+  const handleDecrement = (counterId) => {
+    dispatch(decrement(counterId))
+
   }
 
-  const totalCount = counters.reduce((sum, current)=>sum + current.value, 0)
+  const totalCount = counters.reduce((sum, current) => sum + current.value, 0)
   return (
     <div className="w-screen h-screen p-10 bg-gray-100
        text-slate-700">
@@ -50,7 +27,7 @@ function App() {
       </h1>
       <div className="max-w-md
         mx-auto mt-10 space-y-5">
-        {counters.map(counter=><Counter key={counter.id} count={counter.value} onIncrement={()=>handleIncrement(counter.id)} onDecrement={()=>handleDecrement(counter.id)}/>
+        {counters.map(counter => <Counter key={counter.id} count={counter.value} onIncrement={() => handleIncrement(counter.id)} onDecrement={() => handleDecrement(counter.id)} />
         )}
         <Starts totalCount={totalCount} />
       </div>
